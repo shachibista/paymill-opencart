1f5c7b9d971d2277d3f5008f67d72a2bb1a8da8b
Shachi Bista <shachibista@gmail.com>
2013-10-25 17:48:21 +0200

    Made paymill form customisable through themes

--- paymill/catalog/controller/paymill.php
+++ paymill/catalog/controller/paymill.php
@@ -82,7 +82,10 @@ abstract class ControllerPaymentPaymill extends Controller implements Services_P
         }
 
         $this->data['paymill_activepayment'] = $this->getPaymentName();
-        $this->template = 'default/template/payment/paymill.tpl';
+				$this->template = 'default/template/payment/paymill.tpl';
+				if(file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/paymill.tpl')){
+					$this->template = DIR_TEMPLATE . $this->config->get('config_template') . '/template/payment/paymill.tpl';
+				}
 
         $this->render();
     }
